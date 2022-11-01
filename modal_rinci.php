08caa4e515bcab643e1b306f2ea535055ece6bea
gtpesantren22 <cakromat26@gmail.com>
2022-11-01 13:19:29 +0700

    Updates

--- modal_rinci.php
+++ modal_rinci.php
@@ -30,7 +30,7 @@ $df = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM modal WHERE kode = '
                 <div class="col-lg-3 col-md-6">
                     <div class="ibox bg-success color-white widget-stat">
                         <div class="ibox-body">
-                            <h2 class="m-b-5 font-strong">0</h2>
+                            <h3 class="m-b-5 font-strong">0</h3>
                             <div class="m-b-5">Pemasukan</div><i class="ti-user widget-stat-icon"></i>
                             <div><a href="santri.php" class="small-box-footer color-white ">
                                     Lihat Selengkapnya <i class="fa fa-arrow-circle-right color-white"></i>
@@ -42,7 +42,7 @@ $df = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM modal WHERE kode = '
                 <div class="col-lg-3 col-md-6">
                     <div class="ibox bg-danger color-white widget-stat">
                         <div class="ibox-body">
-                            <h2 class="m-b-5 font-strong">108</h2>
+                            <h3 class="m-b-5 font-strong">108</h3>
                             <div class="m-b-5">Pengeluaran</div><i class="ti-user widget-stat-icon"></i>
                             <div><i class="fa fa-level-down m-r-5"></i><small>-12% Lower</small></div>
                         </div>
@@ -51,7 +51,7 @@ $df = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM modal WHERE kode = '
                 <div class="col-lg-3 col-md-6">
                     <div class="ibox bg-warning color-white widget-stat">
                         <div class="ibox-body">
-                            <h2 class="m-b-5 font-strong">$1570</h2>
+                            <h3 class="m-b-5 font-strong">$1570</h3>
                             <div class="m-b-5">Saldo</div><i class="fa fa-money widget-stat-icon"></i>
                             <div><i class="fa fa-level-up m-r-5"></i><small>22% higher</small></div>
                         </div>
@@ -65,5 +65,4 @@ $df = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM modal WHERE kode = '
 
 <?php
 include 'footer.php';
-
 ?>
