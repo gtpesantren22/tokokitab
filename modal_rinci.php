d31895edd8da4595582d6a4367a8f0a97fd9ccec
gtpesantren22 <cakromat26@gmail.com>
2022-11-09 09:58:20 +0700

    statistik

--- modal_rinci.php
+++ modal_rinci.php
@@ -7,6 +7,13 @@ $df = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM modal WHERE kode = '
 
 $masuk = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FROM masuk WHERE kategori = '$kd' "));
 $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FROM keluar WHERE kategori = '$kd' "));
+
+$sql3 = mysqli_query($conn, "SELECT *, SUM(total) as jml FROM detail_jual WHERE kategori = '$kd' ");
+$jual = mysqli_fetch_assoc($sql3);
+$sql4 = mysqli_query($conn, "SELECT *, SUM(total) as jml FROM detail_kolakan WHERE kategori = '$kd' ");
+$kolak = mysqli_fetch_assoc($sql4);
+$sql5 = mysqli_query($conn, "SELECT *, SUM(debet) as jml FROM jasa WHERE kategori = '$kd' ");
+$jasa = mysqli_fetch_assoc($sql5);
 ?>
 
 <div class="page-content fade-in-up">
@@ -54,9 +61,8 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
                         <div class="col-lg-4 col-md-6">
                             <div class="ibox bg-success color-white widget-stat">
                                 <div class="ibox-body">
-                                    <h3 class="m-b-5 font-strong"><?= rupiah($masuk['jml']) ?></h3>
-                                    <div class="m-b-5">Pemasukan</div><i
-                                        class="ti-arrow-circle-up widget-stat-icon"></i>
+                                    <h3 class="m-b-5 font-strong"><?= rupiah($masuk['jml']+$jual['jml']+$jasa['jml']) ?></h3>
+                                    <div class="m-b-5">Pemasukan</div><i class="ti-arrow-circle-up widget-stat-icon"></i>
                                     <div>
                                         <a href="masuk.php" class="small-box-footer color-white ">
                                             Selengkapnya <i class="fa fa-arrow-circle-right color-white"></i>
@@ -68,9 +74,8 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
                         <div class="col-lg-4 col-md-6">
                             <div class="ibox bg-danger color-white widget-stat">
                                 <div class="ibox-body">
-                                    <h3 class="m-b-5 font-strong"><?= rupiah($keluar['jml']) ?></h3>
-                                    <div class="m-b-5">Pengeluaran</div><i
-                                        class="ti-arrow-circle-down widget-stat-icon"></i>
+                                    <h3 class="m-b-5 font-strong"><?= rupiah($keluar['jml']+$kolak['jml']) ?></h3>
+                                    <div class="m-b-5">Pengeluaran</div><i class="ti-arrow-circle-down widget-stat-icon"></i>
                                     <div><a href="keluar.php" class="small-box-footer color-white ">
                                             Selengkapnya <i class="fa fa-arrow-circle-right color-white"></i>
                                         </a>
@@ -82,7 +87,7 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
                             <div class="ibox bg-warning color-white widget-stat">
                                 <div class="ibox-body">
                                     <h3 class="m-b-5 font-strong">
-                                        <?= rupiah($masuk['jml'] - ($df['nominal'] + $keluar['jml'])); ?></h3>
+                                        <?= rupiah(($masuk['jml'] + $jual['jml'] + $jasa['jml']) - ($df['nominal'] + $keluar['jml'] + $kolak['jml'])); ?></h3>
                                     <div class="m-b-5">Saldo</div><i class="fa fa-money widget-stat-icon"></i>
                                     <div><small>Termasuk modal utama</small></div>
                                 </div>
@@ -95,8 +100,7 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
             <div class="row">
                 <div class="col-md-7">
                     <center><b><u>Rincian Debet & Kredit</u></b></center>
-                    <table class="table table-striped table-bordered table-hover table-sm" id="example-table"
-                        cellspacing="0" width="100%">
+                    <table class="table table-striped table-bordered table-hover table-sm" id="example-table" cellspacing="0" width="100%">
                         <thead>
                             <tr>
                                 <th>No</th>
@@ -112,48 +116,61 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
                             $sql = mysqli_query($conn, "SELECT * FROM masuk WHERE kategori = '$kd' ");
                             while ($data = mysqli_fetch_assoc($sql)) {
                             ?>
-                            <tr>
-                                <td><?= $no++; ?></td>
-                                <td><?= $data['tanggal']; ?></td>
-                                <td><?= rupiah($data['nominal']); ?></td>
-                                <td class="text-success"><b><i class="fa fa-arrow-circle-up"></i> Pemasukan</b></td>
-                                <td>Pemasukan</td>
-                            </tr>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td><?= $data['tanggal']; ?></td>
+                                    <td><?= rupiah($data['nominal']); ?></td>
+                                    <td class="text-success"><b><i class="fa fa-arrow-circle-up"></i> Pemasukan</b></td>
+                                    <td>Pemasukan</td>
+                                </tr>
                             <?php }
                             $sql2 = mysqli_query($conn, "SELECT * FROM keluar WHERE kategori = '$kd' ");
                             while ($data2 = mysqli_fetch_assoc($sql2)) {
                             ?>
-                            <tr>
-                                <td><?= $no++; ?></td>
-                                <td><?= $data2['tanggal']; ?></td>
-                                <td><?= rupiah($data2['nominal']); ?></td>
-                                <td class="text-danger"><b><i class="fa fa-arrow-circle-down"></i> Pengeluaran</b></td>
-                                <td>Pengeluaran</td>
-                            </tr>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td><?= $data2['tanggal']; ?></td>
+                                    <td><?= rupiah($data2['nominal']); ?></td>
+                                    <td class="text-danger"><b><i class="fa fa-arrow-circle-down"></i> Pengeluaran</b></td>
+                                    <td>Pengeluaran</td>
+                                </tr>
 
                             <?php }
-                            $sql3 = mysqli_query($conn, "SELECT * FROM detail_jual WHERE kategori = '$kd' ");
+$sql3 = mysqli_query($conn, "SELECT * FROM detail_jual WHERE kategori = '$kd' ");
                             while ($data2 = mysqli_fetch_assoc($sql3)) {
                             ?>
-                            <tr>
-                                <td><?= $no++; ?></td>
-                                <td>-</td>
-                                <td><?= rupiah($data2['total']); ?></td>
-                                <td class="text-success"><b><i class="fa fa-arrow-circle-up"></i> Pemasukan</b></td>
-                                <td>Penjualan</td>
-                            </tr>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td>-</td>
+                                    <td><?= rupiah($data2['total']); ?></td>
+                                    <td class="text-success"><b><i class="fa fa-arrow-circle-up"></i> Pemasukan</b></td>
+                                    <td>Penjualan</td>
+                                </tr>
 
                             <?php }
-                            $sql4 = mysqli_query($conn, "SELECT * FROM detail_kolakan WHERE kategori = '$kd' ");
+$sql4 = mysqli_query($conn, "SELECT * FROM detail_kolakan WHERE kategori = '$kd' ");
                             while ($data2 = mysqli_fetch_assoc($sql4)) {
                             ?>
-                            <tr>
-                                <td><?= $no++; ?></td>
-                                <td>-</td>
-                                <td><?= rupiah($data2['total']); ?></td>
-                                <td class="text-danger"><b><i class="fa fa-arrow-circle-down"></i> Pengeluaran</b></td>
-                                <td>Kolakan</td>
-                            </tr>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td>-</td>
+                                    <td><?= rupiah($data2['total']); ?></td>
+                                    <td class="text-danger"><b><i class="fa fa-arrow-circle-down"></i> Pengeluaran</b></td>
+                                    <td>Kolakan</td>
+                                </tr>
+
+                            <?php } 
+                            
+$sql5 = mysqli_query($conn, "SELECT * FROM jasa WHERE kategori = '$kd' ");
+                            while ($data2 = mysqli_fetch_assoc($sql5)) {
+                            ?>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td>-</td>
+                                    <td><?= rupiah($data2['debet']); ?></td>
+                                    <td class="text-success"><b><i class="fa fa-arrow-circle-up"></i> Pemasukan</b></td>
+                                    <td>Jasa</td>
+                                </tr>
 
                             <?php } ?>
 
@@ -162,7 +179,7 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
                 </div>
 
                 <div class="col-md-5">
-
+                    <center><b><u>Statistik Modal</u></b></center>
                     <!-- <canvas id="bar-example" width="400" height="400"></canvas> -->
                     <div id="bar-example"></div>
                     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script> -->
@@ -175,20 +192,25 @@ $keluar = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(nominal) as jml FRO
                     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/prettify/r224/prettify.min.css">
                     <link rel="stylesheet" href="morris/morris.css">
                     <script>
-                    var masuk = <?php $masuk['jml'] ?>;
-                    Morris.Bar({
-                        element: 'bar-example',
-                        data: [{
-                            y: '<?= $df['nama'] ?>',
-                            Pemasukan: masuk,
-                            Pengeluaran: 90,
-                            Penjualan: 90,
-                            Kolakan: 90
-                        }],
-                        xkey: 'y',
-                        ykeys: ['Pemasukan', 'Pengeluaran', 'Penjualan', 'Kolakan'],
-                        labels: ['Pemasukan', 'Pengeluaran', 'Penjualan', 'Kolakan']
-                    });
+                        var masuk = Number(<?= $masuk['jml'] ?>);
+                        var keluar = Number(<?= $keluar['jml'] ?>);
+                        var jual = Number(<?= $jual['jml'] ?>);
+                        var kolak = Number(<?= $kolak['jml'] ?>);
+                        var jasa = Number(<?= $jasa['jml'] ?>);
+                        Morris.Bar({
+                            element: 'bar-example',
+                            data: [{
+                                y: '<?= $df['nama'] ?>',
+                                Pemasukan: masuk,
+                                Pengeluaran: keluar,
+                                Penjualan: jual,
+                                Kolakan: kolak,
+                                Jasa: jasa,
+                            }],
+                            xkey: 'y',
+                            ykeys: ['Pemasukan', 'Pengeluaran', 'Penjualan', 'Kolakan', 'Jasa'],
+                            labels: ['Pemasukan', 'Pengeluaran', 'Penjualan', 'Kolakan', 'Jasa']
+                        });
                     </script>
                 </div>
             </div>
