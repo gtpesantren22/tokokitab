5eb17698848cb223e9a4ca6764d510a498aaa1d4
gtpesantren22 <cakromat26@gmail.com>
2025-08-19 19:58:57 +0700

    update: db

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,6 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "db_usaha");
-// $conn = mysqli_connect("localhost", "u9048253_dwk", "PesantrenDWKIT2021", "u9048253_kitab");
-
 $sentral = mysqli_connect("localhost", "root", "", "db_sentral");
-// $conn = mysqli_connect("localhost", "u9048253_dwk", "PesantrenDWKIT2021", "u9048253_sentral");
 
 function rupiah($angka)
 {
